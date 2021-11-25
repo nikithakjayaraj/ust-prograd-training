08e170006dd63735d0b61e4cde87ca6e0b627ec3
Nikitha K Jayaraj <nikithakjayaraj@gmail.com>
2021-11-25 19:14:32 +0530

    added solution to Playing with cubes

---
+++ day3/Playing with cubes.java
@@ -0,0 +1,16 @@
+public class Cube{
+  // note: use primitive data type for the side of the cube
+  private int Side=0;
+  
+  public int getSide()
+  {
+      return Side;
+
+  }  
+  public void setSide(int num)
+  {
+    Side=num;
+  }
+  
+  
+}
